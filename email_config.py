4aa0a643627824185eebc813cc81d82c6f1d88f5
Edward Lee <edward@edldesigns.com>
2015-05-09 23:51:39 +0200

    Basic app to be daemonized. Gets and stores ip address in a txt file, checks current against strored, sends email if ip changes.

---
+++ email_config.py
@@ -0,0 +1,8 @@
+# Email config
+import smtplib
+
+sender = 'your.wwp@gmail.com'
+receiver = 'to.wwp@email.com'
+username = 'your.wwp@gmail.com'
+password = '********'
+server = smtplib.SMTP('smtp.gmail.com:587')
---
+++ storeip.py
@@ -0,0 +1,44 @@
+import urllib, re, os, smtplib 
+from email_config import *
+
+# Check file to exists if not create it to store ip address  
+filename = 'current_ip.txt'
+if not os.path.isfile(filename):
+	file(filename, 'w').close()
+
+# Get your current ip 
+current_ip = re.search('"([0-9.]*)"', urllib.urlopen("http://ip.jsontest.com/").read()).group(1)
+
+# Open stored ip address
+stored_ip = open(filename)
+read_ip = stored_ip.read()
+stored_ip.close()
+
+# test
+# current_ip = "201.2.64.06"
+
+message = "\r\n".join(["From: edlee@gmail@gmail.com",
+"To: edlee.wwp@gmail.com",
+"Subject: ip Change",
+"",
+"Your ip address has changed.\nNew ip address is %s" % current_ip
+])
+
+# Check stored address against current address
+if read_ip == current_ip:
+	print("ip not changed")
+	print(read_ip)
+else:
+	print("ip has changed")
+	print(current_ip)
+	write_ip = open(filename, 'w')
+	write_ip.write(current_ip)
+	write_ip.close()
+	try:
+		server.ehlo()
+		server.starttls()
+		server.login(username,password)
+		server.sendmail(sender, receiver, message)
+		print("Successfully sent")
+	except:
+		print"Error: unable to send mail"
